fb293dbae95452335205d0c0458e92ebb85449b4
Anis Boubaker <anis@boubaker.ca>
2020-09-24 08:55:25 -0400

    Initialisation du projet

---
+++ Groue02/Cours03/main.c
@@ -0,0 +1,16 @@
+/*
+ * TCH009-02 - Cours 03
+ *
+ * Exemples présententés en cours
+ *
+ * Auteur: Anis Boubaker
+ * Date: 2020-09-24
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Hello World!");
+}
