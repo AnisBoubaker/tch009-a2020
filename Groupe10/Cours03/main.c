c925948068d7fc8b0458757efbb9d76ee3b3c9a0
Anis Boubaker <anis@boubaker.ca>
2020-09-24 13:56:14 -0400

    Laboratoire note #2

--- Groupe10/Cours03/main.c
+++ Groupe10/Cours03/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(void) {
+    /*
+    //Coreection du laboratoire noté #2
+    unsigned int valeur; //Valeur saisie par l'usager
+    unsigned int chiffre;  //Ce qui remplacera le 3e chiffre
+    unsigned int chiffre_supprime; //Le chiffre a extraire du nombre
+    unsigned int resultat;
+    unsigned int quotient; //Stocke le résultat des divisions /10
+
+    printf("Saisir une valeur: ");
+    scanf("%d", &valeur);
+
+    printf("Saisir le nouveau 3e chiffre: ");
+    scanf("%d", &chiffre);
+
+    //On isole le chiffre à supprimer
+    quotient = valeur / 100;
+    chiffre_supprime = quotient % 10;
+
+    //On remplace le chiffre par le nouveau
+    resultat = valeur - (chiffre_supprime*100);
+    resultat = resultat + (chiffre * 100);
+
+    printf("Résultat: %d", resultat);
+    */
+    return EXIT_SUCCESS;
 }
