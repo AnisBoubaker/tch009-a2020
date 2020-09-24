a4f95605d0418cf97c078db73c744894ff89e683
Anis Boubaker <anis@boubaker.ca>
2020-09-24 08:59:35 -0400

    Init

---
+++ Groue02/Cours3a/main.c
@@ -0,0 +1,15 @@
+/*
+ * TCH009-02 - Cours3
+ *
+ * Exemples présentés durant le cours
+ *
+ * Date: 2020-09-24
+ */
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
