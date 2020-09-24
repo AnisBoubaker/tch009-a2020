ea2cd0c81b02f9fdf2dab24e5666ce0d9e584fa6
Anis Boubaker <anis@boubaker.ca>
2020-09-24 12:42:30 -0400

    Évaluation des expressions

--- Groue02/Cours3a/main.c
+++ Groue02/Cours3a/main.c
@@ -13,11 +13,93 @@
 
 int main() {
 
+    int x;
+
+    printf("Saisir une valeur: ");
+    scanf("%d", &x);
+
+    if(x != 0) //Équivalent à dire if(x)
+    {
+        printf("Traitement 1\n");
+    }
+    else
+    {
+        printf("Traitement 2\n");
+    }
+
+
+    /*
+    int x;
+    int y;
+
+    x = 25;
+
+    y = (x > 0) + 45;
+
+    printf("y vaut: %d\n", y);
+    */
+
+
+    /*int x ;
+
+    x = 0;
+
+    if(x)
+    {
+        printf("La condition est vraie!");
+    }
+    */
+
+    /*
+    int choix;
+
+    printf("MENU PRINCIPAL: \n\n");
+    printf("1- Nouveau document\n");
+    printf("2- Ouvrir un document existant\n");
+    printf("3- Quitter\n");
+    printf(" >> ");
+    scanf("%d", &choix);
+
+
+
+    switch(choix)
+    {
+        case 1:
+            printf("D'accord, nous créons un nouveau document!\n");
+            break;
+        case 2:
+            printf("Très bien, nou ouvrons un nouveau document!\n");
+            break;
+        case 3:
+            printf("Au revoir!\n");
+            break;
+        default:
+            printf("Mauvaise touche!\n");
+    }*/
+
+    /*
+    if(choix == 1)
+    {
+        printf("D'accord, nous créons un nouveau document!");
+    }
+    else if(choix == 2)
+    {
+        printf("Très bien, nou ouvrons un nouveau document!");
+    }
+    else if(choix == 3)
+    {
+        printf("Au revoir!");
+    }
+    else
+    {
+        printf("Vous avez tapé sur une mauvaise touche!");
+    }*/
+
     /*
      * Demander à l'utilisateur 3 nombres
      * Afficher le maximum des 3 nombres saisis
      */
-    double nb1, nb2, nb3; //3 nombres saisis par l'usager
+    /*double nb1, nb2, nb3; //3 nombres saisis par l'usager
     double max; // Valeur maximale parmi les 3 nombres siaiss
 
     printf("Saisir le 1er nombre: ");
@@ -68,7 +150,7 @@ int main() {
         }
     }*/
 
-    printf("Le maximum parmi %lf, %lf et %lf est : %lf\n", nb1, nb2, nb3, max);
+    //printf("Le maximum parmi %lf, %lf et %lf est : %lf\n", nb1, nb2, nb3, max);
 
 
     /*
