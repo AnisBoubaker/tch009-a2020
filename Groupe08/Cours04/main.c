ee86ee30b424ed17145ec90df6cfe64ff54ea201
Anis Boubaker <anis@boubaker.ca>
2020-09-28 13:21:35 -0400

    Début du cours.

---
+++ Groupe08/Cours04/main.c
@@ -0,0 +1,13 @@
+/*
+ * TCH009-08 - Cours 04
+ * Exemples présentés durant le cours
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
