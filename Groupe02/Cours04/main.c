955e49b2bd8f3764eccd027059d58ea45bf3ba43
Anis Boubaker <anis@boubaker.ca>
2020-10-01 09:57:23 -0400

    Somme harmonique

--- Groupe02/Cours04/main.c
+++ Groupe02/Cours04/main.c
@@ -9,5 +9,34 @@
 
 int main(void) {
 
+    //Programme qui calcule une puissance d'un réel à un exposant entier positif
 
+
+
+
+    /*int dernier_terme; //Dernier terme de la série harmonique calculee
+    int compteur;
+    double somme;
+
+    printf("Saisir le dernier terme: ");
+    scanf("%d", &dernier_terme);
+
+    // A ne pas faire à la maison!
+    while(15)
+    {
+        printf("Je suis dans une boucle infinie!\n");
+    }
+
+
+
+    compteur = 1;
+    somme = 0;
+    while (compteur <= dernier_terme){
+        // 1/1 + 1/2 + 1/3 + ... + 1/n
+        somme = somme + 1.0/compteur;
+        compteur++; //compteur = compteur + 1;
+    }
+
+    printf("Somme des termes: %lf\n", somme);
+    */
 }
