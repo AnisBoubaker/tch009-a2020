7eecabcc93babee63e153e26023f7234341fd78b
Anis Boubaker <anis@boubaker.ca>
2020-10-08 11:25:57 -0400

    Début de programme de jeu.

--- Groupe02/Cours05/main.c
+++ Groupe02/Cours05/main.c
@@ -18,6 +18,28 @@ int nb_aleatoire(int min, int max)
     return min + (int)(rand() / (RAND_MAX + 0.001) * (max - min + 1));
 }
 
+int somme_chiffres(int nb)
+{
+    int quotient_10;
+    int somme;
+
+    quotient_10 = nb;
+    while( quotient_10 > 0 )
+    {
+        somme += quotient_10 % 10;
+        quotient_10 /= 10; //quotient_10 = quotient_10 / 10
+    }
+
+    return somme;
+}
+
+
+/*
+ * Créer la fonction valeur absolue qui retourne la valeur absolue d'un nombre.
+ */
+
+
+
 int main() {
     int nombre_genere; //Nombre tiré aléatoirement
     int nombre_saisi; //Réponse de l'usager
@@ -27,42 +49,51 @@ int main() {
     int quotient_10; //Stocke les divisiions successives par 10 lors calcul
 
     unsigned int t1; //Temps t1
-
+    unsigned int t2; //temps t2
 
     srand((unsigned int)time(NULL));
     rand();
 
 
-    t1 = (unsigned int)time(NULL);
-    printf("Ce que m'a donné la fonction time: %u\n", t1);
-
     nombre_genere = nb_aleatoire(1, 50000);
 
-    quotient_10 = nombre_genere;
+
+    /*quotient_10 = nombre_genere;
     while( quotient_10 > 0 )
     {
         somme_genere += quotient_10 % 10;
         quotient_10 /= 10; //quotient_10 = quotient_10 / 10
-    }
+    }*/
+
+    somme_genere = somme_chiffres(nombre_genere);
 
     //printf("La somme des chiffres de %d: %d\n", nombre_genere, somme_genere);
 
     printf("Saisir un nombre dont la somme des chiffres egale celle de %d: ", nombre_genere);
+    t1 = (unsigned int)time(NULL);
     scanf("%d", &nombre_saisi);
+    t2 = (unsigned int)time(NULL);
 
-    quotient_10 = nombre_saisi;
+    /*quotient_10 = nombre_saisi;
     while( quotient_10 > 0 )
     {
         somme_saisi += quotient_10 % 10;
         quotient_10 /= 10; //quotient_10 = quotient_10 / 10
-    }
+    }*/
+
+    somme_saisi = somme_chiffres(nombre_saisi) + 5 * 2;
 
     //printf("La somme des chiffres de %d: %d\n", nombre_saisi, somme_saisi);
 
-    if(somme_genere == somme_saisi)
+    if(somme_genere == somme_saisi && nombre_saisi!=nombre_genere && t2-t1<=10)
     {
         printf("Bravo!!\n");
-    } else
+    }
+    else if(somme_genere == somme_saisi && nombre_saisi!=nombre_genere && t2-t1>10)
+    {
+        printf("Trop lent...\n");
+    }
+    else
     {
         printf("Mauvaise réponse!\n");
     }
