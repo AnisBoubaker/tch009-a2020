b2d30f326fd548ed886cc24a3c5f2da78bf736a6
Anis Boubaker <anis@boubaker.ca>
2020-10-08 17:02:45 -0400

    Fin du cours.

--- Groupe10/Cours05/main.c
+++ Groupe10/Cours05/main.c
@@ -13,32 +13,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-int nb_aleatoire(int min, int max)
-{
-    return min + (int)(rand() / (RAND_MAX + 0.001) * (max - min + 1));
-}
+//  Déclaration des fonctions //
 
-//Ecrire une fonction qui calcule la valeur absolue d'un réel (val_abs)
-
-
-int somme_chiffres(int nb)
-{
-    int somme = 0;
-    int quotient;
-
-    quotient = nb;
-    while(quotient>0)
-    {
-        somme += quotient%10;
-        quotient /= 10;
-    }
-
-    return somme;
-}
+/*
+ * CALCULER_DOUBLE
+ * Calcule le double d'un entier.
+ * ENTREES:
+ * - x (entier): le nombre pour lequel on calcule le double
+ * SORTIE:
+ * (double) le double de la valeur reçue.
+ */
+int calculer_double(int x);
 
 
 int main(void)
 {
+    int x = 25;
+
+
+    x = calculer_double(x);
+
+    printf("%d", x);
+
+
+
+    /*
+
+
     int nombre_tire; //Nombre aléatoire obtenu
     int quotient; //Quotient des divisions par 10
     int somme_tire;
@@ -46,8 +47,18 @@ int main(void)
     int somme_saisi; //Somme des chiffres du nombre saisi
     unsigned int t1, t2; //Deux temps pour mesurer le temps de réponse
 
+    double x, y;
+
     srand((unsigned int)time(NULL));
     rand();
+
+    x = -134;
+    y = -25;
+
+    x = val_abs( x );
+    y = val_abs( y );
+
+    //printf("%lf\n", val_abs(0 ) );
 
 
     nombre_tire = nb_aleatoire(1, 100000);
@@ -62,7 +73,7 @@ int main(void)
     }*/
 
     //printf("%d => %d", nombre_tire, somme_tire);
-
+/*
     printf("Saisir un nombre dont la somme des chiffres est la même que:\n");
     printf("%d : ", nombre_tire);
     t1 = (unsigned int)time(NULL);
@@ -79,7 +90,7 @@ int main(void)
         quotient /= 10;
     }*/
 
-
+/*
     if(somme_tire == somme_saisi && t2-t1<=10)
     {
         printf("Bravo!\n");
@@ -92,6 +103,55 @@ int main(void)
     {
         printf("Mauvaise réponse!\n");
     }
-
+*/
     return EXIT_SUCCESS;
 }
+
+
+
+int nb_aleatoire(int min, int max)
+{
+    return min + (int)(rand() / (RAND_MAX + 0.001) * (max - min + 1));
+}
+
+//Ecrire une fonction qui calcule la valeur absolue d'un réel (val_abs)
+double val_abs(double val)
+{
+    if(val < 0)
+    {
+        val = -val;
+    }
+
+    return val;
+}
+
+int somme_chiffres(int nb)
+{
+    int somme = 0;
+    int quotient;
+
+    quotient = nb;
+    while(quotient>0)
+    {
+        somme += quotient%10;
+        quotient /= 10;
+    }
+
+    return somme;
+}
+
+int calculer_double(int x)
+{
+    x = x*2;
+    return x;
+}
+
+
+void afficher_menu( void )
+{
+    printf("1- ouvrir un docu\n");
+    printf("2- noveau document\n");
+    printf("3- quitter\n");
+}
+
+
