3e2455a4fb5ea056aa2dc514c3d134c3030c4c34
Anis Boubaker <anis@boubaker.ca>
2020-10-26 15:31:29 -0400

    Cours 8

--- Groupe08/Cours08/main.c
+++ Groupe08/Cours08/main.c
@@ -2,28 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
-double pow_entier(double n, int puiss)
-{
-    double resultat = 1;
-    int i=0; //compteur
-    int puiss_pos;
 
-    puiss_pos = abs(puiss);
-    for(i=0; i < puiss_pos; i++)
-    {
-        resultat *= n;
-    }
-
-
-    if(puiss < 0)
-    {
-        resultat = 1 / resultat;
-    }
-
-    return resultat;
-}
-
-
+/*
+ * POW_ENTIER
+ * Calcule la
+ */
+double pow_entier(double n, int puiss);
 
 int main() {
 
@@ -48,3 +32,32 @@ int main() {
 
     return 0;
 }
+
+double pow_entier(double n, int puiss)
+{
+    double resultat = 1;
+    int i=0; //compteur
+    int puiss_pos;
+
+    puiss_pos = abs(puiss);
+    for(i=0; i < puiss_pos; i++)
+    {
+        resultat *= n;
+    }
+
+
+    if(puiss < 0)
+    {
+        resultat = 1 / resultat;
+    }
+
+    return resultat;
+}
+
+/*
+ * Une fonction qui affiche la table de multiplication
+ * 1    2   3   4   5   ... 10
+ * 2    4   6
+ * ..
+ * 10   20  30
+ */
