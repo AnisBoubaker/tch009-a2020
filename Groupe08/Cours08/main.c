ffcaf6f5b4e38a70aa0faca9ce49a93961b0fd34
Anis Boubaker <anis@boubaker.ca>
2020-10-26 14:00:47 -0400

    Cours 8

---
+++ Groupe08/Cours08/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    int nb=0;
+    double val = 0 ;
+    double t = 0;
+    //Saisir: -1, -1, 10, 35, 15, -1
+
+    while (nb-1==0 || val >=0 )
+    {
+        if (val >= 0)
+        {
+            t +=val;
+        }
+        else
+        {
+            nb--;
+        }
+        printf("Valeur: ");
+        scanf("%lf", &val);
+        nb++;
+    }
+
+    printf("Résultat: %lf\n", t / (nb-1) );
+
+
+    return 0;
+}
