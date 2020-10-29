6e8403808c03a2f360a3cd2734b91604a40f5831
Anis Boubaker <anis@boubaker.ca>
2020-10-29 14:30:38 -0400

    Cours 8

--- Groupe10/Cours08/main.c
+++ Groupe10/Cours08/main.c
@@ -1,27 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+double approx_pi(int nb_termes);
+
 int main() {
-    int nb=0;
-    double val=0;
-    double t=0;
 
-    while(nb-1==0 || val>=0)
-    {
-        if(val>=0)
-        {
-            t+=val; // t = t + val;
-        }
-        else
-        {
-            nb--;
-        }
-        printf("Valeur: ");
-        scanf("%lf", &val);
-        nb++;
-    }
-
-    printf("Résultat: %lf\n", t / (nb-1) );
+    printf("%lf\n", approx_pi(1000000));
 
     return EXIT_SUCCESS;
 }
+
+
+double approx_pi(int nb_termes)
+{
+    int i;
+    double resultat;
+    int signe;
+
+    resultat = 0;
+    signe = 1;
+    for(i = 1; i < 2*nb_termes ; i+=2)
+    {
+        resultat+= signe * (1.0 / i);
+        signe *=-1;
+    }
+
+    return resultat * 4;
+}
+
+
+/*int nb=0;
+    double val;
+    double t=0;
+
+    do
+    {
+        printf("Valeur: ");
+        scanf("%lf", &val);
+
+        if(val>=0)
+        {
+            t+=val; // t = t + val;
+            nb++;
+        }
+    } while(nb==0 || val>=0) ;
+
+    printf("Résultat: %lf\n", t / nb );*/
