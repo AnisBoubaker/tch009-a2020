1a4b0c2935a596c57912e2e0f099122b27fce274
Anis Boubaker <anis@boubaker.ca>
2020-10-29 13:25:24 -0400

    Cours 8

--- Groupe10/Cours08/main.c
+++ Groupe10/Cours08/main.c
@@ -1,37 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    int nb=0;
-    double val = 0 ;
-    double t = 0;
-    //Saisir: -1, -1, 10, 35, 15, -1
-    do{
-        printf("Valeur: ");
-        scanf("%lf", &val);
-        if(val>=0)
-        {
-            t +=val;
-            nb++;
-        }
-    } while(!nb || val>=0);
-    
-
-
-    /*while (nb-1==0 || val >=0 )
-    {
-        if (val >= 0)
-        {
-            t +=val;
-        }
-        else
-        {
-            nb--;
-        }
-        printf("Valeur: ");
-        scanf("%lf", &val);
-        nb++;
-    }*/
-
-    printf("Résultat: %lf\n", t / (nb-1) );
-
+    printf("Hello, World!\n");
+    return 0;
 }
