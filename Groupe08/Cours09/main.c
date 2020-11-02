82a24aebc6daa1ba1d66cfe74471f968e07d9d63
Anis Boubaker <anis@boubaker.ca>
2020-11-02 15:10:05 -0500

    Cours9

--- Groupe08/Cours09/main.c
+++ Groupe08/Cours09/main.c
@@ -8,9 +8,51 @@
  *
  */
 
+#define NOMBRE_ETUDIANTS_MAX 20
 
 
 int main() {
+    double notes[20]; //Initialiser un tableau à des 0 partout
+    int mon_tab[] = {10, 20, 30};
+    int i;
+    double saisie;
+    double moyenne;
+    int nb_notes; //Taille effective du tableau
+
+    printf("Saisir les notes, -1 pour terminer\n");
+    nb_notes=0;
+    do{
+        printf("Saisir la note de l'étudiant: ");
+        scanf("%lf", &saisie);
+        if(saisie!=-1)
+        {
+            notes[nb_notes] = saisie;
+            nb_notes++;
+        }
+    } while(saisie!=-1);
+
+    moyenne = 0;
+    for(i=0; i<nb_notes; i++)
+    {
+        moyenne+=notes[i];
+    }
+    moyenne /= nb_notes;
+
+    printf("La moyenne: %lf\n", moyenne);
+
+    /*
+    for(i=0; i<20; i++)
+    {
+        notes[i] = 10;
+    }
+
+    for(i=0; i<20; i++)
+    {
+        printf("Note à l'indice: %d: %lf\n", i, notes[i]);
+    }*/
+
+
+    /*
     double notes[5];
     double somme;
     int i;
@@ -30,6 +72,6 @@ int main() {
     }
 
     printf("La somme des notes est: %lf\n", somme);
-
+    */
     return 0;
 }
