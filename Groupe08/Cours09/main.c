02a9f29d846d79e10bfbfae1d8af601c5ab50838
Anis Boubaker <anis@boubaker.ca>
2020-11-02 14:07:48 -0500

    Cours9

--- Groupe08/Cours09/main.c
+++ Groupe08/Cours09/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
+/*
+ * Programme qui lit 5 notes au clavier
+ * qui les stocke dans un tableau.
+ * Après avoir lu, calculer la somme de toutes
+ * les notes et afficher la somme.
+ *
+ */
+
+
+
 int main() {
-    printf("Hello, World!\n");
+    int notes[3] = {89, 78, 100};
+    int note_session_passee[3];
+    int i;
+
+    printf("%d\n", notes[1] );
+
+    notes[1] = 65;
+
+    printf("%d\n", notes[1] );
+
+    for(i=0; i<3; i++)
+    {
+        notes[i] = 0;
+    }
+
+    printf("Contenu du tableau: \n");
+    for(i=0; i<3; i++)
+    {
+        printf("%d\n", notes[i]);
+    }
+
+    for(i=0; i<3 ; i++)
+    {
+        note_session_passee[i] = notes[i];
+    }
+
     return 0;
 }
