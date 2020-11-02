bc7a9a5a8c18173e0561091db15b6d7f89661832
Anis Boubaker <anis@boubaker.ca>
2020-11-02 12:59:57 -0500

    Cours9

--- Groupe10/Cours08/main.c
+++ Groupe10/Cours08/main.c
@@ -1,33 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mathematiques.h"
 
-
-double approx_pi(int nb_termes);
-
-
-/*
- * afficher_table_mult
- *
- * Affiche la table de multiplication pour toutes les valeurs jusqu'à max.
- * Exemple avec un max = 10
- *
- * 1 2 3 4 5 ... 10
- * 2 4 6 8 ....  20
- * ...
- * ...
- * 9 18 27 ..... 90
- * 10 20 30 .... 100
- *
- * ENTREES:
- * - max (entier) : jusqu'où aller dans la table?
- * SORTIE: Aucune (affichage seulement)
- *
- */
 
 
 int main() {
-
     printf("%lf\n", approx_pi(1000000));
+    afficher_table_mult(10);
 
     return EXIT_SUCCESS;
 }
@@ -39,38 +18,5 @@ int main() {
 
 
 
-double approx_pi(int nb_termes)
-{
-    int i;
-    double resultat;
-    int signe;
-
-    resultat = 0;
-    signe = 1;
-    for(i = 1; i < 2*nb_termes ; i+=2)
-    {
-        resultat+= signe * (1.0 / i);
-        signe *=-1;
-    }
-
-    return resultat * 4;
-}
 
 
-/*int nb=0;
-    double val;
-    double t=0;
-
-    do
-    {
-        printf("Valeur: ");
-        scanf("%lf", &val);
-
-        if(val>=0)
-        {
-            t+=val; // t = t + val;
-            nb++;
-        }
-    } while(nb==0 || val>=0) ;
-
-    printf("Résultat: %lf\n", t / nb );*/
