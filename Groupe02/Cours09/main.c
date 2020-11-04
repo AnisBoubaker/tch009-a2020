07045933d933cd6da7c3cf28ffdb20d918f4527d
Anis Boubaker <anis@boubaker.ca>
2020-11-04 10:31:42 -0500

    Cours 9

--- Groupe02/Cours09/main.c
+++ Groupe02/Cours09/main.c
@@ -6,53 +6,97 @@
 * les notes et afficher la somme.
 */
 
+/*
+ * Exercice:
+ * Écrire un programme qui déclare un tableau de taille 100,
+ * qui y stocke les 100 premiers termes de la suite de Fibonacci puis qui les affiche.
+
+ Définition: la suite de Fibonacci se définit récursivement
+Fib(0) = 0
+Fib(1) = 1
+Pour n>1, Fib(n) = Fib(n-2)+Fib(n-1)
+*/
+
+//0 1 1 2 3 5 8 13 ....
 
 
 
 int main() {
-
-    //Tableau notes de 5 entiers
-    int notes[5] = {89, 78, 89, 100, 67};
-    double notes_intra[3] = {100, 67.5, 80};
-
-    int note1 = 0;
-    int note2;
-    int note3;
-    int note4;
-    int note5;
-    int i;
+    int tab[100];
+    int toto= 10;
 
 
-    /*printf("Case 3: %d\n",  notes[3] );
-    printf("Case 1: %d\n",  notes[1] );
 
-    notes[1] = 82;
+    printf("%d", toto);
 
-    printf("Case 1: %d\n",  notes[1] );
-    */
 
-    for(i=0; i<5; i++)
+    //FAUX - Syntaxiquement Faux
+    //notes = {0, 10, 80};
+    /*for(i=0; i<100; i++)
     {
-        printf("Saisir la note %d: \n", i);
-        scanf("%d", &notes[i] );
+        notes[i] = 1;
+    }*/
+
+
+    /*for(i=0; i<5; i++)
+    {
+        printf("Saisir la note %d: ", i+1);
+        scanf("%lf", &notes[i]);
     }
 
+    somme = 0;
     for(i=0; i<5; i++)
     {
-        printf("%d\t", notes[i]);
+        somme += notes[i];
     }
 
-
-    /*printf("Saisir la note 1: \n");
-    scanf("%d", &note1);
-    printf("Saisir la note 2: \n");
-    scanf("%d", &note2);
-    printf("Saisir la note 3: \n");
-    scanf("%d", &note3);
-    printf("Saisir la note 4: \n");
-    scanf("%d", &note4);
-    printf("Saisir la note 5: \n");
-    scanf("%d", &note5);
+    printf("La somme des notes: %lf.\n", somme);
+    printf("La moyenne: %lf\n", somme/5);
     */
     return 0;
 }
+
+
+//Tableau notes de 5 entiers
+/*int notes[5] = {89, 78, 89, 100, 67};
+double notes_intra[3] = {100, 67.5, 80};
+
+int note1 = 0;
+int note2;
+int note3;
+int note4;
+int note5;
+int i;
+*/
+
+/*printf("Case 3: %d\n",  notes[3] );
+printf("Case 1: %d\n",  notes[1] );
+
+notes[1] = 82;
+
+printf("Case 1: %d\n",  notes[1] );
+*/
+/*
+for(i=0; i<5; i++)
+{
+printf("Saisir la note %d: \n", i);
+scanf("%d", &notes[i] );
+}
+
+for(i=0; i<5; i++)
+{
+printf("%d\t", notes[i]);
+}
+*/
+
+/*printf("Saisir la note 1: \n");
+scanf("%d", &note1);
+printf("Saisir la note 2: \n");
+scanf("%d", &note2);
+printf("Saisir la note 3: \n");
+scanf("%d", &note3);
+printf("Saisir la note 4: \n");
+scanf("%d", &note4);
+printf("Saisir la note 5: \n");
+scanf("%d", &note5);
+*/
