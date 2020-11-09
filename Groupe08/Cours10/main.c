052582cfd8a4e18ad1c83273c0faa6c4357f9c41
Anis Boubaker <anis@boubaker.ca>
2020-11-09 14:05:58 -0500

    Cours10

--- Groupe08/Cours10/main.c
+++ Groupe08/Cours10/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+ * min_tab
+ * Ecrire une fonction qui prend en paramètre
+ * un tableau d'entiers, et qui retourne la valeur
+ * minimale dans le tableau.
+ */
+
+
 int main() {
     printf("Hello, World!\n");
     return 0;
