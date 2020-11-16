2c3420946728d6e150803ebe03d779cff5460244
anis <anis@boubaker.ca>
2020-11-16 16:43:30 -0500

    Cours11

--- Groupe08/Cours11/main.c
+++ Groupe08/Cours11/main.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
 
+#define MAX_LIGNES 10
+#define MAX_COLONNES 10
+
+void afficher_tab2d(int tab[][MAX_COLONNES], int nb_lignes, int nb_colonnes)
+{
+    for(int ligne =0; ligne<nb_lignes; ligne++)
+    {
+        for (int colonne = 0; colonne < nb_colonnes; colonne++) {
+            printf("%d\t", tab[ligne][colonne]);
+        }
+        printf("\n");
+    }
+}
+
+double moy_tab2d(int tab[][MAX_COLONNES], int nb_lignes, int nb_colonnes)
+{
+    double moyenne = 0 ;
+    for(int i=0; i<nb_lignes; i++)
+    {
+        for(int j=0; j<nb_colonnes; j++)
+        {
+            moyenne+=tab[i][j];
+        }
+    }
+    moyenne /= nb_lignes*nb_colonnes;
+    return moyenne;
+}
+
 int main() {
 
     //Tableau à 2 dimensions d'entiers - 4 lignes et 5 colonnes
-    int tab1_2d[4][5];
+    int tab1_2d[MAX_LIGNES][MAX_COLONNES];
 
     //Tableau à 2 dimensions (3 lignes, 8 colonnes) initilisé
-    int tab2_2d[3][8] = { {3, 5, 10}, {6, 10, 3, 12} };
+    int tab2_2d[MAX_LIGNES][MAX_COLONNES] = { {3, 5, 10}, {6, 10, 3, 12} };
+    double moyenne;
+
     /*
      * 3  5  10  0  0  0  0  0
      * 6 10   3 12  0  0  0  0
      * 0  0   0  0  0  0  0  0
      */
+    afficher_tab2d(tab2_2d, 2, 5);
 
     printf("La case ayant pour indice 0,2: %d\n", tab2_2d[0][2]);
     printf("La case ayant pour indice 1,3: %d\n", tab2_2d[1][3]);
 
-    for(int ligne =0; ligne<3; ligne++)
-    {
-        for (int colonne = 0; colonne < 8; colonne++) {
-            printf("%d\t", tab2_2d[ligne][colonne]);
-        }
-        printf("\n");
-    }
 
+    printf("La moyenne : %lf\n",  moy_tab2d(tab2_2d, 2, 5) );
 
-
+    return 0;
 }
