6209e9d788eac013b510705a955ceb6dd71e7774
anis <anis@boubaker.ca>
2020-11-19 09:08:57 -0500

    Cours11

--- Groupe02/Cours11/main.c
+++ Groupe02/Cours11/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+ * Écrire une fonction qui reçoit un tableau (int)
+ * et qui retourne le maximum et l'indice du maximum.
+ *
+ */
+
+
 int main() {
     printf("Hello, World!\n");
     return 0;
