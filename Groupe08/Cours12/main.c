58ba7f6b7ca602c84bf8280ffe148eff70696542
anis <anis@boubaker.ca>
2020-11-23 15:03:46 -0500

    Cours 12

--- Groupe08/Cours12/main.c
+++ Groupe08/Cours12/main.c
@@ -11,7 +11,7 @@ int main() {
 
     //Créer une chaine de caractères
     //char une_chaine[8]={'B', 'o', 'n', 'j', 'o', 'u', 'r', '\0'}
-    char une_chaine2[] = "Bonjour";
+    char une_chaine2[] = "Bonjour, j'adore coder en langage C!";
     // "Bonjour" <==> {'B', 'o', 'n', 'j', 'o', 'u', 'r', '\0'}
 
     //%s : (String) Afficher une chaine de caractères
@@ -26,12 +26,39 @@ int main() {
     }
     printf("La chaine %s contient %d caracteres.\n", une_chaine2, nb_caracteres);
 
-    //Calculer le nombre de caractères alphabétiques de la chaine de caractère une_chaine2
-    //a->z, A->Z
-
+    //Une autre façon de compter les caractères (i contiendra le nombre de caractères)
     int i;
     for(i=0; une_chaine2[i]!='\0'; i++)
         ;
+
+
+
+    //Calculer le nombre de caractères alphabétiques de la chaine de caractère une_chaine2
+    //a->z, A->Z
+    nb_caracteres = 0;
+    i = 0;
+    while(une_chaine2[i]!='\0')
+    {
+        //Majuscules
+        if(une_chaine2[i]>='A' && une_chaine2[i]<='Z')
+        {
+            nb_caracteres++;
+        }
+        //Minuscule
+        if(une_chaine2[i]>='a' && une_chaine2[i]<='z')
+        {
+            nb_caracteres++;
+        }
+        i++;
+    }
+    printf("Le nombre de caractères alphabétiques de %s est: %d", une_chaine2, nb_caracteres);
+
+
+
+
+
+
+
 
     /*for(int i=0; i<7; i++)
     {
