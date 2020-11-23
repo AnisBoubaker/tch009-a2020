337e0426b2d5b8aa9e22930c9919f79233e7ef9f
anis <anis@boubaker.ca>
2020-11-23 13:51:26 -0500

    Cours 11

--- Groupe08/Cours12/main.c
+++ Groupe08/Cours12/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+#define TAILLE_MAX_CHAINE 250
+
 int main() {
-    printf("Hello, World!\n");
+    int a =10;
+    //(5, 15, 20, 35)
+    int tab[10] = {5, 15, 20, 35};
+
+    //Créer une chaine de caractères
+    char une_chaine[TAILLE_MAX_CHAINE]={'B', 'o', 'n', 'j', 'o', 'u', 'r'};
+
+    //%s : (String) Afficher une chaine de caractères
+    printf("%s", une_chaine);
+
+
+    /*for(int i=0; i<7; i++)
+    {
+        printf("%c", une_chaine[i]);
+    }*/
+
+
+
     return 0;
 }
