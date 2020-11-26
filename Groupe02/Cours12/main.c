d82562a660f5a022b867f1392f05e5cdf81ca0e4
anis <anis@boubaker.ca>
2020-11-26 10:32:32 -0500

    Cours12

--- Groupe02/Cours12/main.c
+++ Groupe02/Cours12/main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX_SAISIE 250
+
+//Un tableau de cacactères est en réalité un
+//pointeur vers le premier caractère de l'espace
+//réservé au tableau. Les deux notations sont
+//équivalenetes;
+int taille_chaine(const char* chaine);
+
+int taille_chaine(const char chaine[])
+{
+    int taille = 0;
+    while(chaine[taille]!='\0')
+    {
+        taille ++;
+    }
+    //Interdit par l'utilisation du mot clé
+    //const sur le paramètre.
+    //chaine[0] = 'A';
+    return taille;
+}
+
 
 int main() {
     //Déclarer une chaine de caractères:
@@ -9,6 +30,8 @@ int main() {
     char une_chaine[9] = "Bonjour\n";
     char saisie[MAX_SAISIE];
     int taille;
+    int taille_calculee;
+    int i;
 
     printf("%s", une_chaine);
 
@@ -25,6 +48,15 @@ int main() {
     printf("Taille de la chaine saisie: %d\n", taille);
 
     //Calculer de la chaine de caractères sans utiliser strlen.
+    /*taille_calculee=0;
+    i=0;
+    while(saisie[i]!='\0')
+    {
+        taille_calculee++;
+        i++;
+    }*/
+    taille_calculee = taille_chaine(saisie);
+    printf("Taille de la saisie, calculée sans strlen: %d\n", taille_calculee);
 
 
     //Écraser le \n avec un \0:
